4ea44efcb53ed784e7f8894fbe89ece6af861287
Protanker25 <Protanker25@gmail.com>
2020-11-06 01:33:06 +1000

    Добавьте файлы проекта.

---
+++ Main_client.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NC_Client_Alpha
+{
+    /// <summary>
+    /// Логика взаимодействия для Main_client.xaml
+    /// </summary>
+    public partial class Main_client : Window
+    {
+        public Main_client()
+        {
+            InitializeComponent();
+        }
+        private void GetImages(string path, List<BitmapImage> list)
+        {
+            DirectoryInfo folder = new DirectoryInfo(path);
+            if (folder.Exists)
+            {
+                foreach (FileInfo fileInfo in folder.GetFiles())
+                {
+                    if (".jpg|.jpeg|.png".Contains(fileInfo.Extension.ToLower()))
+                    {
+                        BitmapImage src = new BitmapImage();
+                        src.BeginInit();
+                        src.UriSource = new Uri(fileInfo.FullName, UriKind.Absolute);
+                        src.EndInit();
+                        list.Add(src);
+                    }
+                }
+            }
+        }
+    }
+}
