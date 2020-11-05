90ab3b6b0bc99ae6abdec520f1157fffec87cf8d
Protanker25 <Protanker25@gmail.com>
2020-11-06 02:44:21 +1000

    ПоправилБаг

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace NC_Client_Alpha
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void LoadSettings(string path)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("users.xml");
+            // получим корневой элемент
+            XmlElement xRoot = xDoc.DocumentElement;
+            // обход всех узлов в корневом элементе
+            foreach (XmlNode xnode in xRoot)
+            {
+                // получаем атрибут name
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("name");
+                    if (attr != null)
+                        Console.WriteLine(attr.Value);
+                }
+                // обходим все дочерние узлы элемента user
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    // если узел - company
+                    if (childnode.Name == "company")
+                    {
+                        Console.WriteLine("Компания: {0}", childnode.InnerText);
+                    }
+                    // если узел age
+                    if (childnode.Name == "age")
+                    {
+                        Console.WriteLine("Возраст: {0}", childnode.InnerText);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+
+
+        }
+        private void GetImages(string path, List<BitmapImage> list)
+        {
+            DirectoryInfo folder = new DirectoryInfo(path);
+            if (folder.Exists)
+            {
+                foreach (FileInfo fileInfo in folder.GetFiles())
+                {
+                    if (".jpg|.jpeg|.png".Contains(fileInfo.Extension.ToLower()))
+                    {
+                        BitmapImage src = new BitmapImage();
+                        src.BeginInit();
+                        src.UriSource = new Uri(fileInfo.FullName, UriKind.Absolute);
+                        src.EndInit();
+                        list.Add(src);
+                    }
+                }
+            }
+        }
+
+        private void GetTexts(string path, List<string> list)
+        {
+            string line;
+            using (StreamReader stream = new StreamReader(path))
+            {
+                while ((line = stream.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
+            }
+        }
+    }
+}
